fe645b0c5ec738cafb9b389b79bbd529a5955b40
grabRookie <61619694+grabRookie@users.noreply.github.com>
2023-05-09 23:13:23 +0300

    Update solution.js
    Task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -21,7 +21,7 @@
 //    "data": {
 //      "client": "Иван +7(987)65-43-210",
 //      "order": {
-//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв. 53",
+//        "address": "ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53",
 //        "sum": 900
 //       },
 //       "goods": [
@@ -33,21 +33,35 @@
 //    }
 // }
 
+
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    let data = {
+        client: '', 
+        order: {
+            //  address: address,
+            //   sum: sum,
+        }, 
+        goods: [
+            // {
+            //  title: this.title,
+            //  count: this.count,
+            //}    
+        ]
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    let countOfGoods = goods.length;
+    
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
+    }
+   // data.goods.shift();
+        
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
 
-    data.client = 'Иван';
+    data.client = name + ' ' + phone;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
